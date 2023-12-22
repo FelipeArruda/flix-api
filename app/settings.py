7906d8fece7cfc53f930aca5e50e93b0b681f9b7
Felipe Aparecido Arruda <ctl_farruda@uolinc.com>
2023-12-22 14:52:15 -0300

    fix railway

--- app/settings.py
+++ app/settings.py
@@ -118,6 +118,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# para produção
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
